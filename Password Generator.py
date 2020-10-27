a4ad3af2e7a7a96451abb6a9d3d12d75db8ad83c
gagashsan <72636499+gagashsan@users.noreply.github.com>
2020-10-27 07:01:25 -0700

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 06:22:41 2020
+
+# =============================================================================
+# PASSWORD GENERATOR
+# =============================================================================
+
+
+
+@author: gagashsan
+"""
+import random
+
+num_of_pass = int(input('Enter the number of password you would like to get? :  '))
+pass_length = int(input('Enter the length of the password you would like to get? :  '))
+
+    
+chars = 'abcdefghijklmnopqrstuvwxyz123456789!//\\'
+
+for i in range(num_of_pass):
+    password = ' '
+    for p in range(pass_length):
+        password += random.choice(chars)
+        
+        
+    print(password)
+
+    
